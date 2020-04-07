b3d28bfd4c4355da6905e6fb26d28f5c6cfba869
secoil <secoil@hotmail.com>
2020-04-07 09:26:16 +0200

    Johannes Initial Files

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	#region MainMenu ButtonMethods
+
+	public void ScatterPlot()
+	{
+		SceneManager.LoadScene("ScatterPlot");
+	}
+
+	public void ParallelCoordinatePlot()
+	{
+		SceneManager.LoadScene("ParallelCoordinatePlot");
+	}
+
+	public void ScatterPlotMatrix()
+	{
+		SceneManager.LoadScene("ScatterPlotMatrix");
+	}
+
+	public void ValfriTeknik()
+	{
+		SceneManager.LoadScene("ValfriTeknik");
+	}
+
+	public void QuitApplication()
+	{
+		Debug.Log("QUIT...");
+		Application.Quit();
+	}
+
+	#endregion
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+	public static bool applicationIsPaused = false;
+
+	public static GameObject pauseMenuUI;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (applicationIsPaused)
+			{
+				Resume();
+			}
+			else
+			{
+				Pause();
+			}
+		}
+	}
+
+	public void Resume()
+	{
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+
+		applicationIsPaused = false;
+	}
+
+	private void Pause()
+	{
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+
+		applicationIsPaused = true;
+	}
+
+	public void LoadMenu()
+	{
+		Debug.Log("Loading Menu...");
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void QuitApplication()
+	{
+		Debug.Log("Quitting Application...");
+		Application.Quit();
+	}
+}

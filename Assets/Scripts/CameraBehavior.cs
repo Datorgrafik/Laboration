ee4234787fbe967386d95fe107a39f5bb33c9d86
secoil <secoil@hotmail.com>
2020-04-08 14:56:32 +0200

    More Cleanup

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -4,40 +4,40 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour {
 
-    public float speed;
-    private Vector3 startPosition;
+	public float speed;
+	private Vector3 startPosition;
 
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start()
+	{
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Camera direction
-        if (Input.GetMouseButton(1))
-        {
-            float mouseX = Input.GetAxis("Mouse X") * 4;
-            float mouseY = -(Input.GetAxis("Mouse Y") * 4);
-            transform.Rotate(0, mouseX, 0, Space.World);
-            transform.Rotate(mouseY, 0, 0, Space.Self);
-        }
+	// Update is called once per frame
+	void Update()
+	{
+		//Camera direction
+		if (Input.GetMouseButton(1))
+		{
+			float mouseX = Input.GetAxis("Mouse X") * 4;
+			float mouseY = -(Input.GetAxis("Mouse Y") * 4);
+			transform.Rotate(0, mouseX, 0, Space.World);
+			transform.Rotate(mouseY, 0, 0, Space.Self);
+		}
 
-        if (Input.GetKey(KeyCode.W))
-            transform.position = transform.position + Camera.main.transform.forward * speed;
-        else if (Input.GetKey(KeyCode.S))
-            transform.position = transform.position + Camera.main.transform.forward * speed * -1;
+		if (Input.GetKey(KeyCode.W))
+			transform.position = transform.position + Camera.main.transform.forward * speed;
+		else if (Input.GetKey(KeyCode.S))
+			transform.position = transform.position + Camera.main.transform.forward * speed * -1;
 
-        if (Input.GetKey(KeyCode.A))
-            transform.position = transform.position + Camera.main.transform.right * speed * -1;
-        else if (Input.GetKey(KeyCode.D))
-            transform.position = transform.position + Camera.main.transform.right * speed;
+		if (Input.GetKey(KeyCode.A))
+			transform.position = transform.position + Camera.main.transform.right * speed * -1;
+		else if (Input.GetKey(KeyCode.D))
+			transform.position = transform.position + Camera.main.transform.right * speed;
 
-        if (Input.GetKey(KeyCode.Space))
-            transform.position = transform.position + Camera.main.transform.up * speed;
-        else if (Input.GetKey(KeyCode.LeftControl))
-            transform.position = transform.position + Camera.main.transform.up * speed * -1;
-    }
+		if (Input.GetKey(KeyCode.Space))
+			transform.position = transform.position + Camera.main.transform.up * speed;
+		else if (Input.GetKey(KeyCode.LeftControl))
+			transform.position = transform.position + Camera.main.transform.up * speed * -1;
+	}
 }

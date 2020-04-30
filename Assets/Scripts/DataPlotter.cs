ebd5fa172d3eada1fc2722a72de57460cc04d694
Alex Laurila <s181927@student.hb.se>
2020-04-30 10:09:15 +0200

    Knn camera + targeting

--- Assets/Scripts/DataPlotter.cs
+++ Assets/Scripts/DataPlotter.cs
@@ -55,6 +55,7 @@ public class DataPlotter : MonoBehaviour
 	public static DataPlotter ThisInstans;
 	public static DataClass dataClass;
 	private int selectedIndex = -1;
+    private bool teleportCamera = false;
 
 	#endregion
 
@@ -209,9 +210,26 @@ public class DataPlotter : MonoBehaviour
 				selectedIndex = -1;
 			}
 		}
-		// GameObject newBall = (GameObject)pointList.Last()["DataBall"] as GameObject;
-		//transform.LookAt(newBall.transform);
-	}
+
+        if (ThisInstans.teleportCamera)
+        {
+            ThisInstans.teleportCamera = false;
+            GameObject newBall = (GameObject)pointList.Last()["DataBall"] as GameObject;
+            Camera.main.transform.position = new Vector3(newBall.transform.position.x + 2.5f, newBall.transform.position.y + 1.5f, newBall.transform.position.z - 2.5f);
+            Camera.main.transform.LookAt(newBall.transform);
+
+            if (TargetingScript.selectedTarget != null)
+            {
+                TargetingScript.selectedTarget.GetComponent<Renderer>().material.color = TargetingScript.colorOff;
+                TargetingScript.selectedTarget.transform.localScale += new Vector3(-0.01f, -0.01f, -0.01f);
+            }
+
+            TargetingScript.selectedTarget = newBall;
+            TargetingScript.colorOff = TargetingScript.selectedTarget.GetComponent<Renderer>().material.color;
+            TargetingScript.selectedTarget.GetComponent<Renderer>().material.color = Color.white;
+            TargetingScript.selectedTarget.transform.localScale += new Vector3(+0.01f, +0.01f, +0.01f);
+        }
+    }
 
 	public static void ChangeColor(GameObject dataPoint, int targetFeatureIndex)
 	{
@@ -269,6 +287,7 @@ public class DataPlotter : MonoBehaviour
 
 
 		pointList.Add(newDataPoint);
+        ThisInstans.teleportCamera = true;
 
 		ThisInstans.PlottData();
 		Blink(KNN.kPoints);

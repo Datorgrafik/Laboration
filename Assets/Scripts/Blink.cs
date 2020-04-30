8b384c062b7e647e024dc5496e19f141d4f7b21e
Elin Järdemar <s184327@student.hb.se>
2020-04-30 09:05:00 +0200

    färg blink

--- Assets/Scripts/Blink.cs
+++ Assets/Scripts/Blink.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class Blink : MonoBehaviour
 {
     Renderer Ball;
+    Color color;
 
     // Update is called once per frame
     void Start()
     {
         Ball = GetComponent<Renderer>();
         InvokeRepeating("Blinking",0,0.2f);
+        color = Ball.material.color;
     }
     void Blinking()
     {
-        if (Ball.enabled == true)
+        if (Ball.material.color == color)
         {
-            Ball.enabled = false;
+            Ball.material.color = Color.white;
         }
         else
-            Ball.enabled = true;
+            Ball.material.color = color;
     }
 }

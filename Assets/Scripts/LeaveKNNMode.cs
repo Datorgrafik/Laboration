72bc6d52dc60d726e9c1cd0d7a8e1b50304ffedd
secoil <secoil@hotmail.com>
2020-05-13 23:43:36 +0200

    PCP temp fix update

--- Assets/Scripts/LeaveKNNMode.cs
+++ Assets/Scripts/LeaveKNNMode.cs
@@ -6,48 +6,44 @@ using UnityEngine.UI;
 
 public class LeaveKNNMode : MonoBehaviour
 {
+	public Button LeaveKNN;
+	public GameObject KNNWindow;
+	public Button NewData;
 
-    public Button LeaveKNN;
-    public GameObject KNNWindow;
-    public Button NewData;
+	// Start is called before the first frame update
+	void Start()
+	{
+		LeaveKNN.onClick.AddListener(Leave);
+	}
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        LeaveKNN.onClick.AddListener(Leave);
-    }
+	public void Leave()
+	{
+		KNNWindow.SetActive(false);
+		KNN.kPoints.Clear();
 
-    public void Leave()
-    {
-
-        KNNWindow.SetActive(false);
-        KNN.kPoints.Clear();
-
-        if (SceneManager.GetActiveScene().name == "ScatterPlotMatrix")
-        {
-            ScatterPlotMatrix.KNNMode = false;
-            ScatterPlotMatrix.ThisInstans.PlottData();
-        }
-        if (SceneManager.GetActiveScene().name == "ValfriTeknik")
-        {
-            ScatterplotDimensions.KNNMode = false;
-            ScatterplotDimensions.ThisInstans.PlottData();
-        }
-
-        if (SceneManager.GetActiveScene().name == "ParallelCoordinatePlot")
-        {
-            ParallelCoordinatePlotter.KNNMode = false;
-            KNN.kPoints.Clear();
-            ParallelCoordinatePlotter.ThisInstans.ReorderColumns();
-
-        }
-        else
-        {
-            DataPlotter.KNNMode = false;
-            DataPlotter.ThisInstans.PlottData();
-
-        }
-        NewData.interactable = true;
-    }
+		if (SceneManager.GetActiveScene().name == "ScatterPlotMatrix")
+		{
+			ScatterPlotMatrix.KNNMode = false;
+			ScatterPlotMatrix.ThisInstans.PlottData();
+			NewData.interactable = true;
+		}
+		else if (SceneManager.GetActiveScene().name == "ValfriTeknik")
+		{
+			ScatterplotDimensions.KNNMode = false;
+			ScatterplotDimensions.ThisInstans.PlottData();
+			NewData.interactable = true;
+		}
+		else if (SceneManager.GetActiveScene().name == "ParallelCoordinatePlot")
+		{
+			ParallelCoordinatePlotter.KNNMode = false;
+			ParallelCoordinatePlotter.ThisInstans.ReorderColumns();
+		}
+		else
+		{
+			DataPlotter.KNNMode = false;
+			DataPlotter.ThisInstans.PlottData();
+			NewData.interactable = true;
+		}
+	}
 }
-    
+	
--- Assets/Scripts/TargetingScript.cs
+++ Assets/Scripts/TargetingScript.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class TargetingScript : MonoBehaviour
 {
@@ -18,7 +20,6 @@ public class TargetingScript : MonoBehaviour
 	void Update()
 	{
 		//Left mouse click. If object is clicked, target it.
-
 		if (Input.GetMouseButtonDown(0) && !DataPlotter.KNNMode && !ScatterPlotMatrix.KNNMode && !ScatterplotDimensions.KNNMode)
 		{
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -43,6 +44,41 @@ public class TargetingScript : MonoBehaviour
 				selectedTarget = null;
 			}
 		}
+
+		#region Temporary Fix for PCP
+		// Temporary try to fix for PCP (will not look like this forever)
+		if (ParallelCoordinatePlotter.KNNMode && Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			eventSys = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+			bool missTarget = true;
+
+			foreach (RaycastHit hit in Physics.RaycastAll(ray))
+			{
+				if (hit.collider.CompareTag("DataBall"))
+				{
+					if (SceneManager.GetActiveScene().name == "ParallelCoordinatePlot")
+					{
+						// The latest added datapoint should always be the last one in the list, so this should work
+						if (hit.collider.gameObject.GetComponent<StoreIndexInDataBall>().Index.ToString()
+							.Equals((ParallelCoordinatePlotter.ThisInstans.pointList.Count - 1).ToString()))
+						{
+							missTarget = false;
+							SelectDataBall(hit);
+							break;
+						}
+					}
+				}
+			}
+			// Deselect target 
+			if (missTarget == true && !eventSys.IsPointerOverGameObject() && selectedTarget != null)
+			{
+				selectedTarget.GetComponent<Renderer>().material.color = colorOff;
+				selectedTarget.transform.localScale += new Vector3(-0.01f, -0.01f, -0.01f);
+				selectedTarget = null;
+			}
+		}
+		#endregion
 
 	}
 
